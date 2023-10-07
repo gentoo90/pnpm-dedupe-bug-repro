e9a560dfe3c22ca7aa454057cb7bf6eef7f95bde
gentoo90 <gentoo90@gmail.com>
2023-10-07 16:58:21 +0300

    Add webpack-manifest-plugin (broken)
    TypeError: The 'compilation' argument must be an instance of Compilation

--- my-webpack.config.js
+++ my-webpack.config.js
@@ -1,7 +1,13 @@
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+
 function customizeWebpackConfig(config, _) {
   config.entry['my-entrypoint'] = [
     'src/my-entrypoint.ts',
   ];
+
+  config.plugins.push(
+    new WebpackManifestPlugin({})
+  );
 
   return config;
 }
